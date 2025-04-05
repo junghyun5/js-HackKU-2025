ae4d4ddda6a70707b54cbbca8602b3a85d5f4b62
daummm <50vjtpsxm@naver.com>
2025-04-05 09:09:11 -0500

    Update ListGroup.tsx

--- vite-project/src/components/ListGroup.tsx
+++ vite-project/src/components/ListGroup.tsx
@@ -2,12 +2,10 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   items = [];
 
-  const message = items.length === 0 ? <p>No item found</p> : null;
-
   return (
     <>
       <h1>List</h1>
-      {message}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
